49a4881e817b4b6c6c467ccf3683283b2ea32b42
SkyfallWasTaken <mahadkalam1234@gmail.com>
2022-12-17 15:18:47 +0000

    Add compression to Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import compress from 'vite-plugin-compress'
 import WindiCSS from 'vite-plugin-windicss';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [WindiCSS(), sveltekit()]
+	plugins: [WindiCSS(), sveltekit(), compress]
 };
 
 export default config;
